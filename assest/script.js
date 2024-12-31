829d81cb3525103671a7d47ffdbf99dc3db432da
bardia-ra <bcofn3327@gmail.com>
2024-12-31 11:29:31 +0330

    response

--- assest/script.js
+++ assest/script.js
@@ -3,3 +3,10 @@ document.querySelector('.header-close').addEventListener('click', ()=>{
     document.querySelector('nav').style.margin = "0 0 0 0";
     document.querySelector('main').style.top='100px';
 });
+document.addEventListener('DOMContentLoaded', () => {
+    if (window.innerWidth <= 768) { 
+        const img=document.querySelector('#main-img');
+        img.setAttribute('src', 'img/mobile-main.png');
+        
+    }
+});
