8d4c8fa5255e96229afe2f0a6133f1938bcf8b4a
bardia-ra <bcofn3327@gmail.com>
2024-12-31 13:12:31 +0330

    menu update

--- assest/script.js
+++ assest/script.js
@@ -3,10 +3,18 @@ document.querySelector('.header-close').addEventListener('click', ()=>{
     document.querySelector('nav').style.margin = "0 0 0 0";
     document.querySelector('main').style.top='100px';
 });
+
 document.addEventListener('DOMContentLoaded', () => {
     if (window.innerWidth <= 768) { 
         const img=document.querySelector('#main-img');
         img.setAttribute('src', 'img/mobile-main.png');
         
     }
+});
+
+document.querySelector('.nav-bar').addEventListener('click', ()=>{
+    document.querySelector('.side-nav').classList.toggle('active');
+    document.querySelector('.close-bar').classList.toggle('active');
+    document.querySelector('.open-bar').classList.toggle('active');
+    document.querySelector('main').classList.toggle('active');
 });
