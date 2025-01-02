c3f1b60bd6f58563316fb9295aa8f08a4976b3b8
bardia-ra <bcofn3327@gmail.com>
2025-01-03 00:45:03 +0330

    main-update

--- assest/script.js
+++ assest/script.js
@@ -18,3 +18,31 @@ document.querySelector('.nav-bar').addEventListener('click', ()=>{
     document.querySelector('.open-bar').classList.toggle('active');
     document.querySelector('main').classList.toggle('active');
 });
+document.querySelector('#nav-shop').addEventListener('mouseenter', ()=>{
+    document.querySelector('.shop-nav').style.display="block";
+})
+document.querySelector('#nav-shop').addEventListener('mouseleave', ()=>{
+    document.querySelector('.shop-nav').style.display="none";
+})
+document.querySelector('.shop-nav').addEventListener('mouseenter', ()=>{
+    document.querySelector('.shop-nav').style.display="block";
+})
+document.querySelector('.shop-nav').addEventListener('mouseleave', ()=>{
+    document.querySelector('.shop-nav').style.display="none";
+})
+
+document.querySelector('.shop-drop').addEventListener('click', ()=>{
+    document.querySelector('.shop-menu').classList.toggle('active');
+    document.querySelector('.shop-jam').classList.toggle('active');
+    document.querySelector('.shop-menha').classList.toggle('active');
+})
+document.querySelector('.help-drop').addEventListener('click', ()=>{
+    document.querySelector('.help-menu').classList.toggle('active');
+    document.querySelector('.help-jam').classList.toggle('active');
+    document.querySelector('.help-menha').classList.toggle('active');
+})
+document.querySelector('.sherkat-drop').addEventListener('click', ()=>{
+    document.querySelector('.sherkat-menu').classList.toggle('active');
+    document.querySelector('.sherkat-jam').classList.toggle('active');
+    document.querySelector('.sherkat-menha').classList.toggle('active');
+})
